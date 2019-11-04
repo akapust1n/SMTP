87fbd1a7eed6f48020ea86e127eb86528937e146
Alexey Kapustin <djkah11@yandex.ru>
2019-11-04 23:36:24 +0300

    migrate to pthreads

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 CC=gcc
 CFLAGS=-Wall  -std=c99
+LFLAGS = -lpthread
 SOURCESSERVER := $(shell find server -name '*.c')
 
 all: serverO
 
 serverO: $(SOURCESSERVER)
-	$(CC) $(CFLAGS) $(SOURCESSERVER) -o serverO
+	$(CC) $(CFLAGS) $(SOURCESSERVER) $(LFLAGS) -o serverO
 
 clean:
 	rm -rf *.o
--- server/main.c
+++ server/main.c
@@ -1,35 +1,39 @@
 #include <errno.h>
+#include <fcntl.h>
 #include <netinet/in.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
 #include <sys/poll.h>
 #include <sys/socket.h>
 #include <sys/time.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <stdbool.h>
+#include <pthread.h>
+#include <string.h>
 
 #define PORT 10000
+#define MAX_CLIENTS 50
 
-int initSocker(int *listenSocket)
+int initSocker(int* listenSocket)
 {
     int temp = 1;
 
     *listenSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (*listenSocket< 0) {
+    if (*listenSocket < 0) {
         perror("socket() failed");
         return -1;
     }
 
     if (setsockopt(*listenSocket, SOL_SOCKET, SO_REUSEPORT | SO_REUSEADDR,
-                   (char*)&temp, sizeof(temp)) < 0) {
+            (char*)&temp, sizeof(temp))
+        < 0) {
         perror("setsockopt() failed");
         close(*listenSocket);
         return -1;
     }
 
-    if (ioctl(*listenSocket, FIONBIO, (char*)&temp)< 0) {
+    if (ioctl(*listenSocket, FIONBIO, (char*)&temp) < 0) {
         perror("ioctl() failed");
         close(*listenSocket);
         return -1;
@@ -40,7 +44,8 @@ int initSocker(int *listenSocket)
     addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_port = htons(PORT);
     if (bind(*listenSocket,
-             (struct sockaddr*)&addr, sizeof(addr))< 0) {
+            (struct sockaddr*)&addr, sizeof(addr))
+        < 0) {
         perror("bind() failed");
         close(*listenSocket);
         return -1;
@@ -51,31 +56,25 @@ int initSocker(int *listenSocket)
         close(*listenSocket);
         return -1;
     }
-    return  0;
+    return 0;
 }
-int main()
+void* myPoll(void* lSocket)
 {
-    int listenSocket = -1, newSocket = -1;
-    bool invalidStateOfServer = false, resizeArray = false;
+    int listenSocket = *(int*)lSocket;
     bool closeConn = false;
     char buffer[100];
     int timeout = 3 * 60 * 1000; // 3min
+    bool invalidStateOfServer = false, resizeArray = false;
+    int newSocket = -1;
     int nfds = 1, currentSize = 0;
-    struct pollfd fds[100]; //todo check max size
-
-    if(initSocker(&listenSocket) < 0)
-    {
-        perror("Cant open socket");
-        return  -1;
-    }
-
+    struct pollfd fds[100];
     memset(&fds, 0, sizeof(fds));
 
     fds[0].fd = listenSocket;
     fds[0].events = POLLIN;
 
     do {
-        printf("Waiting on poll()...\n");
+        printf("Waiting on poll() at thread %lu \n", pthread_self());
         int returnCode = poll(fds, (nfds_t)nfds, timeout);
 
         if (returnCode < 0) {
@@ -100,15 +99,18 @@ int main()
             }
             if (fds[i].fd == listenSocket) {
                 printf("  Listening socket is readable\n");
-
+                if (nfds >= MAX_CLIENTS) {
+                    printf("reach limit of client\n");
+                    continue;
+                }
                 do {
                     newSocket = accept(listenSocket, NULL, NULL);
                     if (newSocket < 0) {
-                        if (errno != EWOULDBLOCK) {
+                       /* if (errno != EWOULDBLOCK) {
                             perror("  accept() failed");
                             invalidStateOfServer = true;
-                        }
-                        break;
+                        }*/ //todo catch more errors
+                        continue;
                     }
 
                     printf("  New incoming connection - %d\n", newSocket);
@@ -128,7 +130,7 @@ int main()
                     if (len < 0) {
                         if (errno != EWOULDBLOCK) {
                             perror("  recv() failed");
-                            closeConn =  true;
+                            closeConn = true;
                         }
                         break;
                     }
@@ -139,13 +141,13 @@ int main()
                         break;
                     }
 
-                    printf("  %d bytes received\n", len);
+                    printf("  %d bytes received at thread %lu \n", len, pthread_self());
 
                     // todo: remove this
                     returnCode = (int)send(fds[i].fd, buffer, (size_t)len, 0);
                     if (returnCode < 0) {
                         perror("  send() failed");
-                        closeConn =  true;
+                        closeConn = true;
                         break;
                     }
 
@@ -156,7 +158,6 @@ int main()
                     fds[i].fd = -1;
                     resizeArray = true;
                 }
-
             }
         }
 
@@ -179,4 +180,25 @@ int main()
         if (fds[i].fd >= 0)
             close(fds[i].fd);
     }
+    pthread_exit(0);
+}
+int main()
+{
+    int listenSocket = -1;
+
+    if (initSocker(&listenSocket) < 0) {
+        perror("Cant open socket");
+        return -1;
+    }
+    pthread_t tid1, tid2;
+
+    /* создание потока */
+    pthread_create(&tid1, 0, myPoll, &listenSocket);
+    pthread_create(&tid2, 0, myPoll, &listenSocket);
+    printf("Starting thread #1\n");
+    pthread_detach(tid1);
+    printf("Starting thread #2\n");
+    pthread_join(tid2, NULL);
+
+    return  0;
 }
